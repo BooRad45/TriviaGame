4b57dcfa9534aa0b9b13bcb9e46c024793bd2276
Brendan Rogers <brendan@utexas.edu>
2017-03-24 19:58:04 -0500

    beautified js and validated html. Commented out bad code in js and I'm working my way through it.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,86 @@
 $(document).ready(function() {
 
+            $("#startGameButton").click(function() {
+                $("#startGameButton").remove();
+                $("#questions").show();
+
+                //taken from StackOverflow
+                function timer(time, update, complete) {
+                    var start = new Date().getTime();
+                    var interval = setInterval(function() {
+                        var now = time - (new Date().getTime() - start);
+                        if (now <= 0) {
+                            clearInterval(interval);
+                            complete();
+                        } else update(Math.floor(now / 1000));
+                    }, 100); // the smaller this number, the more accurate the timer will be
+                }
+
+                timer(
+                    180000, // milliseconds
+                    function(timeleft) { // called every step to update the visible countdown
+                        document.getElementById("timer").innerHTML = timeleft + " second(s)";
+                    },
+                    function() { // what to do after
+                        $("#timer").replaceWith("<h2>Time is up!<h2>");
+                    });
+
+            });
+
+ });
+
 var numberCorrect = 0;
 var numberWrong = 0;
 
+//////////CONDITIONALS TO TEST FOR CORRECT AND INCORRECT ANSWERS ONCE TIMER IS DONE OR SUBMIT BUTTON IS HIT
+     $(".input").on("click", function() {
 
-//taken from StackOverflow
-    function timer(time,update,complete) {
-    var start = new Date().getTime();
-    var interval = setInterval(function() {
-        var now = time-(new Date().getTime()-start);
-        if( now <= 0) {
-            clearInterval(interval);
-            complete();
-            // call function that is used by submit button as well???How????
-        }
-        else update(Math.floor(now/1000));
-    },100); // the smaller this number, the more accurate the timer will be
+    if  $("input[name="alter"]:checked").val(); {
+    numberCorrect++;
+    console.log(numberCorrect);
 }
-
-timer(
-    180000, // milliseconds
-    function(timeleft) { // called every step to update the visible countdown
-        document.getElementById('timer').innerHTML = timeleft+" second(s)";
-    },
-    function() { // what to do after
-      $( "#timer" ).replaceWith( "<h2>Time is up!<h2>" );
+    else {
+      numberWrong++;
     }
-);
 
-
-
-////////////CONDITIONALS TO TEST FOR CORRECT AND INCORRECT ANSWERS ONCE TIMER IS DONE OR SUBMIT BUTTON IS HIT
-$("#submit").click(function(){ //on submit button click (how to check timer up?)
-
-if ($('input[name=heis]:checked').length > 0) {
+    if  $("input[name="son"]:checked").val(); {
     numberCorrect++;}
     else {
       numberWrong++;
     }
 
-    if ($('input[name=cerPals]:checked').length > 0) {
-    numberCorrect++; }
+    if  $("input[name="nicknames"]:checked").val(); {
+    numberCorrect++;}
+    else {
+      numberWrong++;
+    } 
+
+    if  $("input[name="nicknames"]:checked").val(); {
+    numberCorrect++;}
+    else {
+      numberWrong++;
+    } 
+
+  if  $("input[name="key"]:checked").val(); {
+    numberCorrect++;}
     else {
       numberWrong++;
     }
 
-    if ($('input[name=bluesClue]:checked').length > 0) {
-    numberCorrect++; }
+    if  $("input[name="gray"]:checked").val(); {
+    numberCorrect++;}
     else {
       numberWrong++;
+      }
 
-}
-    if ($('input[name=methyl]:checked').length > 0) {
-    numberCorrect++; }
-    else {
-      numberWrong++;
+    });
 
-}
 
-    if ($('input[name=grayMatter]:checked').length > 0) {
-    numberCorrect++; }
-    else {
-      numberWrong++;
 
-}
-
-};
+// ///////////APPEND RESULTS TO PAGE WHEN SUBMIT BUTTON IS HIT////////////////////////////////
 
 
 
 
 
-///////////APPEND RESULTS TO PAGE WHEN SUBMIT BUTTON IS HIT////////////////////////////////
 
-
-
-
-
-// };
